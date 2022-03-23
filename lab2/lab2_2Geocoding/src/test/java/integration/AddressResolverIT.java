04d2a46be4aaca646c17ece9afea3e6e78dc0835
Vicente <vicente.costa@ua.pt>
2022-03-23 11:03:51 +0000

    lab2 completed

--- lab2/lab2_2Geocoding/src/test/java/integration/AddressResolverIT.java
+++ lab2/lab2_2Geocoding/src/test/java/integration/AddressResolverIT.java
@@ -4,11 +4,13 @@ import connection.TqsBasicHttpClient;
 import geocoding.Address;
 import geocoding.AddressResolver;
 import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -16,16 +18,21 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class AddressResolverIT {
 
 
-    @BeforeEach
-    public void init(){
-    }
+    TqsBasicHttpClient basicHttpClient = new TqsBasicHttpClient();
+    AddressResolver resolver = new AddressResolver(basicHttpClient);
 
     @Test
     public void whenGoodCoordidates_returnAddress() throws IOException, URISyntaxException, ParseException {
 
         //todo
 
-        // repeat the same tests conditions from AddressResolverTest, without mocks
+        //String mockResult = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\\u00A9 2021 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\\u00A9 2021 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":40.640661,\"lng\":-8.656688}},\"locations\":[{\"street\":\"Cais do Alboi\",\"adminArea6\":\"\",\"adminArea6Type\":\"Neighborhood\",\"adminArea5\":\"Gl\\u00F3ria e Vera Cruz\",\"adminArea5Type\":\"City\",\"adminArea4\":\"\",\"adminArea4Type\":\"County\",\"adminArea3\":\"Centro\",\"adminArea3Type\":\"State\",\"adminArea1\":\"PT\",\"adminArea1Type\":\"Country\",\"postalCode\":\"3800-246\",\"geocodeQualityCode\":\"B1AAA\",\"geocodeQuality\":\"STREET\",\"dragPoint\":false,\"sideOfStreet\":\"N\",\"linkId\":\"0\",\"unknownInput\":\"\",\"type\":\"s\",\"latLng\":{\"lat\":40.640524,\"lng\":-8.656713},\"displayLatLng\":{\"lat\":40.640524,\"lng\":-8.656713},\"mapUrl\":\"http://open.mapquestapi.com/staticmap/v5/map?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&type=map&size=225,160&locations=40.64052368145179,-8.656712986761146|marker-sm-50318A-1&scalebar=true&zoom=15&rand=-963779157\",\"roadMetadata\":null}]}]}";
+        //when(basicHttpClient.doHttpGet(contains("location=40.640661%2C-8.656688"))).thenReturn(mockResult);
+        //e.g.
+        Optional<Address> result = resolver.findAddressForLocation(40.640661, -8.656688);
+
+        //return
+        assertEquals( result, Optional.of(new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) ));
 
     }
 
@@ -34,7 +41,13 @@ public class AddressResolverIT {
 
         //todo
         // repeat the same tests conditions from AddressResolverTest, without mocks
+        //String mockResult = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\u00A9 2022 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\u00A9 2022 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":-300.0,\"lng\":-810.0}},\"locations\":[]}]}";
         
+        //when(basicHttpClient.doHttpGet(contains("location=-300.000000%2C-810.000000"))).thenReturn(mockResult);
+        //e.g.
+        Optional<Address> result = resolver.findAddressForLocation(-300, -810);
+        assertEquals( Optional.empty(),result );
     }
+
 
 }
